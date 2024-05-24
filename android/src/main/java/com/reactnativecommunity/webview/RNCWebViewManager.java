da5892128927878405de4bd44b597175ea23f0cc
Ezenwankwo Gabriel <ezenwankwogabriel@gmail.com>
2024-05-24 16:41:40 +0100

    fix: add imports

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -98,6 +98,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
