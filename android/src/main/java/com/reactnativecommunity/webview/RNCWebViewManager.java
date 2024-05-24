8d12dcadc43fa0234aad064114c286c01ad1a056
Ezenwankwo Gabriel <ezenwankwogabriel@gmail.com>
2024-05-24 14:41:55 +0100

    fix: allow camera permission for whitelisted urls only

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -163,7 +163,12 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
   protected @Nullable String mDownloadingMessage = null;
   protected @Nullable String mLackPermissionToDownloadMessage = null;
 
+  private static Set<String> whitelist;
+
   public RNCWebViewManager() {
+    whitelist = new HashSet<>();
+    whitelist.add("https://alchemy.veriff.com/");
+
     mWebViewConfig = new WebViewConfig() {
       public void configWebView(WebView webView) {
       }
@@ -1054,7 +1059,8 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
       for (String requestedResource : request.getResources()) {
         String androidPermission = null;
 
-        if (requestedResource.equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
+        if (requestedResource.equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)
+            && whitelist.contains(request.getOrigin().toString())) {
           androidPermission = Manifest.permission.CAMERA;
         } else {
           continue;
