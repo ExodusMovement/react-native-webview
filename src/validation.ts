1feeb1ee318ad47cd5cd0cc822b7b3b430c94e06
Jan W <jan.we@exodus.io>
2024-11-28 16:43:17 +0100

    fix: qualify prop path

--- src/validation.ts
+++ src/validation.ts
@@ -4,7 +4,7 @@ import { AndroidWebViewProps, IOSWebViewProps } from './WebViewTypes'
 const validateProps = <P extends IOSWebViewProps | AndroidWebViewProps>(props: P): P => {
   if(props.source && 'html' in props.source){
     const { originWhitelist } = props
-    invariant(originWhitelist  && originWhitelist.length > 0 && !originWhitelist.includes('*'), 'originWhitelist is required when using html prop and cannot include *')
+    invariant(originWhitelist  && originWhitelist.length > 0 && !originWhitelist.includes('*'), 'originWhitelist is required when using source.html prop and cannot include *')
   }
 
   return props
